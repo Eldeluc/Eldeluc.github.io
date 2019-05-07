3adbce80b6a52382653ee37b054ad4be29ac3b57
Eldeluc <Eldeluc@users.noreply.github.com>
2019-05-07 14:09:35 -0700

    Update MainPage.js

--- MainPage.js
+++ MainPage.js
@@ -1,5 +1,13 @@
 window.onload = populateMainPage;
 function populateMainPage(){
     document.getElementById("mainMenu").innerHTML = "Black Diamond Labs";
+    
     alert("Welcome to Black Diamond Labs")
+    if(confirm("Are you a registered user?")){
+        alert("If you say so.")
+    }
+    else{
+        alert("We will have to get you registered.")
+    }
+
 }
