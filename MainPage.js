83204d5452747a589c75fc3149bd676b5ec9284e
Eldeluc <Eldeluc@users.noreply.github.com>
2019-05-07 15:33:48 -0700

    Update MainPage.js

--- MainPage.js
+++ MainPage.js
@@ -1,6 +1,8 @@
 window.onload = populateMainPage;
 function populateMainPage(){
     document.getElementById("mainMenu").innerHTML = "Black Diamond Labs";
+    var username = "\0"
+    var password = "\0"
     
     alert("Welcome to Black Diamond Labs")
     if(confirm("Click 'OK' if you are a registered user.")){
@@ -8,6 +10,12 @@ function populateMainPage(){
     }
     else{
         alert("We will have to get you registered.")
+        username = prompt("Username: ","")
+        password = prompt("Password: ","")
+
+        if((username == "Eldeluc")&&(password == "P13@$3")) {
+            alert("Sign-In Successful ~ Eldeluc")
+        }
     }
 
 }
